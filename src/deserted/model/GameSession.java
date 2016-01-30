d636eda20d490110f3ea6d20e5dfd6c4d9a9628c
S Williams <scottwill@tesco.net>
2016-01-30 18:31:33 +0000

    gameconfig starting stuff

--- src/deserted/model/GameSession.java
+++ src/deserted/model/GameSession.java
@@ -33,10 +33,10 @@ public class GameSession {
 		this.setCompleted(false);
 		this.setCompletionType(0);
 		this.inventory = new Inventory();
-		inventory.addItem(ItemType.FOOD, 10);
-		inventory.addItem(ItemType.WOOD, 25);
-		inventory.addItem(ItemType.METAL, 25);
-		inventory.addItem(ItemType.STONE, 25);
+		inventory.addItem(ItemType.FOOD, GameConfig.STARTING_FOOD);
+		inventory.addItem(ItemType.WOOD, GameConfig.STARTING_WOOD);
+		inventory.addItem(ItemType.METAL, GameConfig.STARTING_METAL);
+		inventory.addItem(ItemType.STONE, GameConfig.STARTING_STONE);
 		
 		this.gameTimer = 0;
 		this.timeSurvived = 0;
