d96f4e2c373369c3b61612b8a10486024073884a
steppers <stepz69@gmail.com>
2016-01-30 11:35:42 +0000

    Changed some stuff in the new graphics

--- src/cheese/graphics/WedgeGFX.java
+++ src/cheese/graphics/WedgeGFX.java
@@ -21,18 +21,17 @@ import org.lwjgl.util.glu.GLU;
 public class WedgeGFX {
     
     // Quad variables
-    private int vaoId = 0;
-    private int vboId = 0;
-    private int vboiId = 0;
+    private int[] vArr;
     private int indicesCount = 0;
     // Shader variables
     private int vsId = 0;
     private int fsId = 0;
     private int pId = 0;
      
-    public WedgeGFX() {        
+    public WedgeGFX() {
+    	vArr = getVertexArray();      
         this.setupQuad();
-        this.setupShaders();
+        this.initShaders();
     }
      
     public void setupQuad() {
@@ -58,23 +57,14 @@ public class WedgeGFX {
         indicesBuffer.flip();
          
         // Create a new Vertex Array Object in memory and select it (bind)
-        vaoId = GL30.glGenVertexArrays();
-        GL30.glBindVertexArray(vaoId);
+        GL30.glBindVertexArray(vArr[0]);
          
-        // Create a new Vertex Buffer Object in memory and select it (bind) - VERTICES
-        vboId = GL15.glGenBuffers();
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vArr[1]);
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
-        GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 32, 0);
-        GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 32, 16);
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-         
-        // Deselect (bind to 0) the VAO
         GL30.glBindVertexArray(0);
          
-        // Create a new VBO for the indices and select it (bind) - INDICES
-        vboiId = GL15.glGenBuffers();
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vArr[2]);
         GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
         GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
     }
@@ -113,12 +103,12 @@ public class WedgeGFX {
         GL20.glUseProgram(pId);
          
         // Bind to the VAO that has all the information about the vertices
-        GL30.glBindVertexArray(vaoId);
+        GL30.glBindVertexArray(vArr[0]);
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
          
         // Bind to the index VBO that has all the information about the order of the vertices
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vArr[2]);
          
         // Draw the vertices
         GL11.glDrawElements(GL11.GL_TRIANGLES, indicesCount, GL11.GL_UNSIGNED_BYTE, 0);
@@ -130,37 +120,59 @@ public class WedgeGFX {
         GL30.glBindVertexArray(0);
         GL20.glUseProgram(0);
     }
+    
+    private int[] getVertexArray(){
+    	int[] r = new int[3];
+    	
+    	// Create a new Vertex Array Object in memory and select it (bind)
+        r[0] = GL30.glGenVertexArrays();
+        GL30.glBindVertexArray(r[0]);
+         
+        // Create a new Vertex Buffer Object in memory and select it (bind) - VERTICES
+        r[1] = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, r[1]);
+        GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 32, 0);
+        GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 32, 16);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+         
+        // Deselect (bind to 0) the VAO
+        GL30.glBindVertexArray(0);
+         
+        // Create a new VBO for the indices and select it (bind) - INDICES
+        r[2] = GL15.glGenBuffers();
+        return r;
+    }
+    
+    private void initShaders() {
+        int errorCheckValue = GL11.glGetError();
+         
+        // Load the vertex shader
+        vsId = this.loadShader("assets/shaders/basic.vs", GL20.GL_VERTEX_SHADER);
+        // Load the fragment shader
+        fsId = this.loadShader("assets/shaders/basic.fs", GL20.GL_FRAGMENT_SHADER);
+         
+        // Create a new shader program that links both shaders
+        pId = GL20.glCreateProgram();
+        GL20.glAttachShader(pId, vsId);
+        GL20.glAttachShader(pId, fsId);
+ 
+        // Position information will be attribute 0
+        GL20.glBindAttribLocation(pId, 0, "in_Position");
+        // Color information will be attribute 1
+        GL20.glBindAttribLocation(pId, 1, "in_Color");
+         
+        GL20.glLinkProgram(pId);
+        GL20.glValidateProgram(pId);
+         
+        errorCheckValue = GL11.glGetError();
+        if (errorCheckValue != GL11.GL_NO_ERROR) {
+            System.out.println("ERROR - Could not create the shaders:" + GLU.gluErrorString(errorCheckValue));
+            System.exit(-1);
+        }
+    }
      
     public void destroyOpenGL() {
-        // Delete the shaders
-        GL20.glUseProgram(0);
-        GL20.glDetachShader(pId, vsId);
-        GL20.glDetachShader(pId, fsId);
-         
-        GL20.glDeleteShader(vsId);
-        GL20.glDeleteShader(fsId);
-        GL20.glDeleteProgram(pId);
-         
-        // Select the VAO
-        GL30.glBindVertexArray(vaoId);
-         
-        // Disable the VBO index from the VAO attributes list
-        GL20.glDisableVertexAttribArray(0);
-        GL20.glDisableVertexAttribArray(1);
-         
-        // Delete the vertex VBO
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-        GL15.glDeleteBuffers(vboId);
-         
-        // Delete the index VBO
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-        GL15.glDeleteBuffers(vboiId);
-         
-        // Delete the VAO
-        GL30.glBindVertexArray(0);
-        GL30.glDeleteVertexArrays(vaoId);
-         
-        Display.destroy();
+        
     }
      
     public int loadShader(String filename, int type) {
